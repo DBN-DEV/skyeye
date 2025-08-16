475366a59e62ea33084e53484065d4382389ee91
hhy <huanghaoyuanhhy@gmail.com>
2025-08-17 00:01:10 +0800

    impl time wheel

--- agent/probe/time_wheel.go
+++ agent/probe/time_wheel.go
@@ -1,9 +1,95 @@
 package probe
 
+import (
+	"encoding/binary"
+	"errors"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
 type timer struct {
+	id        int
 	cancelled bool
 	callback  func()
 }
 
+type slot struct {
+	mu     sync.Mutex
+	timers []timer
+}
+
+// add adds a new timer to the slot and returns offset and id.
+func (s *slot) add(callback func()) (int, int) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	t := timer{id: rand.Int(), callback: callback}
+	s.timers = append(s.timers, t)
+	return len(s.timers) - 1, t.id
+}
+
+func (s *slot) cancel(offset int, id int) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if offset < 0 || offset >= len(s.timers) {
+		return errors.New("probe: offset out of range")
+	}
+
+	if s.timers[offset].id != id {
+		return errors.New("probe: timer ID does not match")
+	}
+
+	s.timers[offset].cancelled = true
+	return nil
+}
+
 type timerWheel struct {
+	ticker time.Ticker
+
+	tick    time.Duration
+	current atomic.Int64
+	slots   []slot
+}
+
+func (tw *timerWheel) Add(delay time.Duration, callback func()) ([]byte, error) {
+	if delay <= 0 {
+		return nil, errors.New("probe: delay must be greater than zero")
+	}
+
+	step := int(delay / tw.tick)
+	if step > len(tw.slots) {
+		return nil, errors.New("probe: delay exceeds timer wheel capacity")
+	}
+
+	current := tw.current.Load()
+	slotNum := int(current + int64(delay/tw.tick)%int64(len(tw.slots)))
+
+	offset, id := tw.slots[slotNum].add(callback)
+
+	// slotNum 8 bytes, id 8 bytes, offset 8 bytes
+	buf := make([]byte, 8*3)
+	binary.BigEndian.PutUint64(buf[0:8], uint64(slotNum))
+	binary.BigEndian.PutUint64(buf[8:16], uint64(offset))
+	binary.BigEndian.PutUint64(buf[16:24], uint64(id))
+
+	return buf, nil
+}
+
+func (tw *timerWheel) Cancel(id []byte) error {
+	if len(id) != 24 {
+		return errors.New("probe: invalid id length")
+	}
+
+	slotNum := binary.BigEndian.Uint64(id[0:8])
+	offset := binary.BigEndian.Uint64(id[8:16])
+	timerID := binary.BigEndian.Uint64(id[16:24])
+
+	if int(slotNum) < 0 || int(slotNum) >= len(tw.slots) {
+		return errors.New("probe: slot number out of range")
+	}
+
+	return tw.slots[slotNum].cancel(int(offset), int(timerID))
 }
---
+++ agent/probe/time_wheel_test.go
@@ -0,0 +1,99 @@
+package probe
+
+import (
+	"encoding/binary"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSlot_add(t *testing.T) {
+	s := &slot{}
+	callback := func() {}
+	offset, id := s.add(callback)
+
+	assert.Equal(t, 0, offset)
+	assert.Equal(t, id, s.timers[0].id)
+}
+
+func TestSlot_cancel(t *testing.T) {
+	s := &slot{}
+	callback := func() {}
+	offset, id := s.add(callback)
+
+	// Cancel with valid offset and id
+	err := s.cancel(offset, id)
+	assert.NoError(t, err)
+	assert.True(t, s.timers[offset].cancelled)
+
+	// Cancel with invalid offset
+	err = s.cancel(-1, id)
+	assert.Error(t, err)
+
+	// Cancel with invalid id
+	err = s.cancel(offset, -1)
+	assert.Error(t, err)
+
+	// Cancel with mismatched id
+	err = s.cancel(offset, id+1)
+	assert.Error(t, err)
+}
+
+func TestTimerWheel_Add(t *testing.T) {
+	tw := &timerWheel{
+		tick:  time.Millisecond,
+		slots: make([]slot, 10),
+	}
+
+	// delay <= 0
+	_, err := tw.Add(0, func() {})
+	assert.Error(t, err)
+	_, err = tw.Add(-time.Millisecond, func() {})
+	assert.Error(t, err)
+
+	// delay exceeds timer wheel capacity
+	_, err = tw.Add(11*time.Millisecond, func() {})
+	assert.Error(t, err)
+
+	// valid delay
+	buf, err := tw.Add(5*time.Millisecond, func() {})
+	assert.NoError(t, err)
+	assert.Len(t, buf, 24)                      // 8 bytes for slotNum, 8bytes for id, 8 bytes for offset
+	assert.Equal(t, 1, len(tw.slots[5].timers)) // One timer should be added to slot 5
+	// slotNum should be 5, offset should be 0, id should eq to the timer's id
+	assert.Equal(t, uint64(5), binary.BigEndian.Uint64(buf[0:8]))
+	assert.Equal(t, uint64(0), binary.BigEndian.Uint64(buf[8:16]))
+	assert.Equal(t, uint64(tw.slots[5].timers[0].id), binary.BigEndian.Uint64(buf[16:24]))
+}
+
+func TestTimerWheel_Cancel(t *testing.T) {
+	tw := &timerWheel{
+		tick:  time.Millisecond,
+		slots: make([]slot, 10),
+	}
+
+	// Cancel with invalid id length
+	err := tw.Cancel([]byte{0, 1, 2})
+	assert.Error(t, err)
+
+	// Cancel with invalid slot number
+	id := make([]byte, 24)
+	binary.BigEndian.PutUint64(id[0:8], 10) // Invalid slot number
+	err = tw.Cancel(id)
+	assert.Error(t, err)
+
+	// Cancel with valid id but no timer exists
+	binary.BigEndian.PutUint64(id[0:8], 0)   // Valid slot number
+	binary.BigEndian.PutUint64(id[8:16], 0)  // Valid offset
+	binary.BigEndian.PutUint64(id[16:24], 1) // Non-existing timer ID
+	err = tw.Cancel(id)
+	assert.Error(t, err)
+
+	// Add a timer and then cancel it
+	buf, err := tw.Add(5*time.Millisecond, func() {})
+	assert.NoError(t, err)
+	err = tw.Cancel(buf)
+	assert.NoError(t, err)
+	assert.True(t, tw.slots[5].timers[0].cancelled)
+}
